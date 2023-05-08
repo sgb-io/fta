b0d1098dd5b4ac49a287278ae204d222341a051f
sgb-io <sam@sgb.io>
2023-05-08 21:00:21 +0100

    Improve accuracy of cyclomatic

--- src/complexity.rs
+++ src/complexity.rs
@@ -1,4 +1,4 @@
-use swc_ecma_ast::Module;
+use swc_ecma_ast::*;
 use swc_ecma_visit::{Visit, VisitWith};
 
 struct ComplexityVisitor {
@@ -12,34 +12,61 @@ impl ComplexityVisitor {
 }
 
 impl Visit for ComplexityVisitor {
-    fn visit_bin_expr(&mut self, expr: &swc_ecma_ast::BinExpr) {
-        let op = expr.op.as_str();
+    fn visit_bin_expr(&mut self, node: &BinExpr) {
+        let op = node.op.as_str();
         if op == "&&" || op == "||" {
             self.complexity += 1;
         }
-        expr.visit_children_with(self);
+        node.visit_children_with(self);
     }
 
-    fn visit_if_stmt(&mut self, stmt: &swc_ecma_ast::IfStmt) {
+    fn visit_if_stmt(&mut self, node: &IfStmt) {
         self.complexity += 1;
-        stmt.visit_children_with(self);
+        node.visit_children_with(self);
     }
 
-    fn visit_switch_stmt(&mut self, stmt: &swc_ecma_ast::SwitchStmt) {
-        self.complexity += 1;
-        stmt.visit_children_with(self);
+    fn visit_switch_stmt(&mut self, node: &SwitchStmt) {
+        // Count each case as a decision point
+        let num_cases: u32 = node.cases.len() as u32;
+        self.complexity += num_cases;
+
+        // Traverse the child nodes (cases and their statements)
+        node.visit_children_with(self);
     }
 
-    fn visit_for_stmt(&mut self, _: &swc_ecma_ast::ForStmt) {
+    fn visit_for_stmt(&mut self, node: &ForStmt) {
         self.complexity += 1;
+        node.visit_children_with(self);
     }
 
-    fn visit_while_stmt(&mut self, _: &swc_ecma_ast::WhileStmt) {
+    fn visit_while_stmt(&mut self, node: &WhileStmt) {
         self.complexity += 1;
+        node.visit_children_with(self);
     }
 
-    fn visit_do_while_stmt(&mut self, _: &swc_ecma_ast::DoWhileStmt) {
+    fn visit_do_while_stmt(&mut self, node: &DoWhileStmt) {
         self.complexity += 1;
+        node.visit_children_with(self);
+    }
+
+    fn visit_for_in_stmt(&mut self, node: &ForInStmt) {
+        self.complexity += 1;
+        node.visit_children_with(self);
+    }
+
+    fn visit_for_of_stmt(&mut self, node: &ForOfStmt) {
+        self.complexity += 1;
+        node.visit_children_with(self);
+    }
+
+    fn visit_catch_clause(&mut self, node: &CatchClause) {
+        self.complexity += 1;
+        node.visit_children_with(self);
+    }
+
+    fn visit_cond_expr(&mut self, node: &CondExpr) {
+        self.complexity += 1;
+        node.visit_children_with(self);
     }
 }
 
