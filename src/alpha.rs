ef867f17c4d24b3d9ed068fe006a9c6fc382f035
sgb-io <sam@sgb.io>
2023-05-08 11:23:31 +0100

    Wip more cases accounted for

--- src/alpha.rs
+++ src/alpha.rs
@@ -49,7 +49,78 @@ impl Visit for AstAnalyzer {
                 }
                 self.total_operands += 1;
             }
-            _ => expr.visit_children_with(self),
+            Expr::Array(array) => {
+                for elem in &array.elems {
+                    if let Some(expr) = elem {
+                        expr.visit_with(self);
+                    }
+                }
+            }
+            Expr::Arrow(arrow) => {
+                // arrow.
+                arrow.params.visit_with(self);
+                arrow.body.visit_with(self);
+            }
+            Expr::Assign(assign) => {
+                self.total_operators += 1;
+                assign.left.visit_with(self);
+                assign.right.visit_with(self);
+            }
+            Expr::Call(call) => {
+                call.callee.visit_with(self);
+                for arg in &call.args {
+                    arg.visit_with(self);
+                }
+            }
+            Expr::Cond(cond) => {
+                cond.test.visit_with(self);
+                cond.cons.visit_with(self);
+                cond.alt.visit_with(self);
+            }
+            Expr::Member(member) => {
+                member.obj.visit_with(self);
+                member.prop.visit_with(self);
+            }
+            Expr::Object(object) => {
+                for prop in &object.props {
+                    match prop {
+                        PropOrSpread::Prop(boxed_prop) => match &**boxed_prop {
+                            Prop::KeyValue(key_value) => {
+                                key_value.key.visit_with(self);
+                                key_value.value.visit_with(self);
+                            }
+                            Prop::Assign(assign) => {
+                                assign.key.visit_with(self);
+                                assign.value.visit_with(self);
+                            }
+                            _ => {}
+                        },
+                        PropOrSpread::Spread(spread) => {
+                            spread.expr.visit_with(self);
+                        }
+                    }
+                }
+            }
+            Expr::Tpl(tpl) => {
+                for expr in &tpl.exprs {
+                    expr.visit_with(self);
+                }
+            }
+            Expr::TsAs(ts_as) => {
+                ts_as.expr.visit_with(self);
+                // No need to visit the type_ann as it doesn't contribute to operands or operators.
+            }
+            Expr::TsNonNull(ts_non_null) => {
+                ts_non_null.expr.visit_with(self);
+            }
+            Expr::Unary(unary) => {
+                self.total_operators += 1;
+                unary.arg.visit_with(self);
+            }
+            _ => {
+                println!("Unhandled expression: {:?}", expr);
+                expr.visit_children_with(self);
+            }
         }
     }
 }
