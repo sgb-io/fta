49e91228d656208c5a29909c7a1e43b16525f4cf
sgb-io <sam@sgb.io>
2023-07-19 09:02:07 +0100

    Update front example, add redux fixture, add publish dry run workflow

---
+++ fixtures/fake-redux/index.ts
@@ -0,0 +1,46 @@
+// functions
+import { createStore, legacy_createStore } from './createStore'
+import combineReducers from './combineReducers'
+import bindActionCreators from './bindActionCreators'
+import applyMiddleware from './applyMiddleware'
+import compose from './compose'
+import __DO_NOT_USE__ActionTypes from './utils/actionTypes'
+
+// types
+// store
+export {
+  Dispatch,
+  Unsubscribe,
+  Observable,
+  Observer,
+  Store,
+  StoreCreator,
+  StoreEnhancer,
+  StoreEnhancerStoreCreator
+} from './types/store'
+// reducers
+export {
+  Reducer,
+  ReducersMapObject,
+  StateFromReducersMapObject,
+  ReducerFromReducersMapObject,
+  ActionFromReducer,
+  ActionFromReducersMapObject,
+  PreloadedStateShapeFromReducersMapObject
+} from './types/reducers'
+// action creators
+export { ActionCreator, ActionCreatorsMapObject } from './types/actions'
+// middleware
+export { MiddlewareAPI, Middleware } from './types/middleware'
+// actions
+export { Action, AnyAction } from './types/actions'
+
+export {
+  createStore,
+  legacy_createStore,
+  combineReducers,
+  bindActionCreators,
+  applyMiddleware,
+  compose,
+  __DO_NOT_USE__ActionTypes
+}
