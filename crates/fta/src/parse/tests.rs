17b663e9216f32d19b27b096c86e3382a06a76ea
Ben <bencollins54@users.noreply.github.com>
2023-10-20 21:37:34 +0100

    Correct test and document why the line count is correct.

--- crates/fta/src/parse/tests.rs
+++ crates/fta/src/parse/tests.rs
@@ -43,6 +43,10 @@ mod tests {
 
     #[test]
     fn it_can_be_configured_to_include_comments_in_the_line_count() {
+        /*
+           The below code includes 10 lines of code, but 12 lines in total due to a leading \n and the \n on like 7.
+           These are filtered regardless of the include_comments flag.
+        */
         let ts_code = r#"
             /*
             Block comment with multiple lines.
@@ -60,6 +64,6 @@ mod tests {
         let (parsed_module, line_count) = parse_module(ts_code, true, true);
 
         assert!(parsed_module.is_ok(), "Failed to parse TypeScript code");
-        assert_eq!(line_count, 11, "Incorrect line count");
+        assert_eq!(line_count, 10, "Incorrect line count");
     }
 }
